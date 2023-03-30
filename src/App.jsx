ae5a47cc88c3a397fbc32e08d8325005aca9d73c
Fernanda Freitas Martinez <fernanda.martinez@integer.pt>
2023-03-30 18:14:00 +0100

    ant form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
+import { Button, Form, Input, Space } from 'antd';
 
 function App() {
+  const onFinish = () => {
+    console.log("Submited")
+  }
+
+  const onFinishFailed = () => {
+    console.log("Failed submition")
+  }
+
   return (
-    <div className="d-flex">
-      <form className='mx-auto mt-5'>
-        <h3 className='mb-4'>Two-factor authentication</h3>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
+    <Form
+      name="basic"
+      style={{ maxWidth: 400 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      layout="vertical"
+      size="large"
+    >
+
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: 'Please input your email!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" size="large" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
 
