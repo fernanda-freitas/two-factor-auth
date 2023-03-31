4c33fee2424214f343c4213a3fac85239929a2b7
Fernanda Freitas Martinez <fernandafreitasmartinez@MBP-de-Fernanda.netcabo.pt>
2023-03-31 18:26:30 +0100

    emailjs

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 function App() {
-  // const [email, setEmail] = useState('')
-  // const [code, setCode] = useState('')
+  const [email, setEmail] = useState('')
+  const [code, setCode] = useState('')
   
   const sendEmail = (e) => {
     e.preventDefault();
+    setCode(Math.floor(Math.random() * 900000) + 100000)
+    console.log(code)
 
-    // const setCode = Math.floor(Math.random() * 900000) + 100000;
 
-    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, {
+      email: {email},
+      code: {code}
+    }, process.env.REACT_APP_PUBLIC_KEY)
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -26,9 +30,11 @@ function App() {
         <form ref={form} onSubmit={sendEmail} className='col-12 col-md-4 mx-auto mt-5 px-5'>
           <h3 className='mb-4'>Two-factor authentication</h3>
           <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email address</label>
-            <input name="user_email" type="email" className="form-control" />
+          
+            <label htmlFor="email" className="form-label">Email</label>
+            <input onChange={(e) => setEmail(e.target.value)} className="form-control" />
             <div className="form-text">We'll never share your email with anyone else.</div>
+          
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
