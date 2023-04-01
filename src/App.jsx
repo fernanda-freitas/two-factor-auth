2186308d8e7a617fe64f5cd3d95f55cf47b18ce8
Fernanda Freitas <idea.fernanda@gmail.com>
2023-04-01 12:35:02 +0100

    trying to handle error messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import emailjs from '@emailjs/browser';
 function App() {
   const form = useRef();
   const [email, setEmail] = useState('')
-  const [emailSentResult, setEmailSentResult] = useState(null)
-  const [emailSentError, setEmailSentError] = useState(null)
+  const [status, setStatus] = useState(null)
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    
     const templateParams = {
       email_to: email,
       generated_code: Math.floor(Math.random() * 900000) + 100000
@@ -17,33 +16,31 @@ function App() {
     
     emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, templateParams, process.env.REACT_APP_PUBLIC_KEY)
       .then((result) => {
-          setEmailSentResult(result.text)
+        setStatus(result.text)
       }, (error) => {
-        setEmailSentError(error.text);
+        setStatus(error.text);
       }); 
   }
 
   return (
     <div className='grid'>
-      {emailSentResult && (
+      {status === 'OK' ? (
         <div className="alert alert-success" role="alert">
-          {emailSentResult}
+          O código foi enviado corretamente.
         </div>
-      )}
-      {emailSentError && (
+      ) : (
         <div className="alert alert-danger" role="alert">
-          {emailSentError}
+          Algo deu errado.
         </div>
-      )}
+        ) 
+      }
       <div className="row">
         <form ref={form} onSubmit={sendEmail} className='col-12 col-md-4 mx-auto mt-5 px-5'>
           <h3 className='mb-4'>Two-factor authentication</h3>
           <div className="mb-3">
-          
             <label htmlFor="email" className="form-label">Email</label>
             <input name="email" onChange={(e) => setEmail(e.target.value)} className="form-control" />
             <div className="form-text">We'll never share your email with anyone else.</div>
-          
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
