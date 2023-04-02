9f6717f8e71c182888620932aae1151102ccf6f6
Fernanda Freitas <idea.fernanda@gmail.com>
2023-04-02 11:11:19 +0100

    adjust form style

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,23 @@ import emailjs from '@emailjs/browser';
 
 function App() {
   const form = useRef();
-  const [email, setEmail] = useState('')
-  const [status, setStatus] = useState(false)
+  const [userEmail, setUserEmail] = useState('')
   const [isFormSent, setIsFormSent] = useState(false)
   const [code, setCode] = useState(['', '', '', '', '', ''])
   
   const sendEmail = (e) => {
     e.preventDefault();
     const templateParams = {
-      email_to: email,
+      email_to: userEmail,
       generated_code: Math.floor(Math.random() * 900000) + 100000
     }
-    
     emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, templateParams, process.env.REACT_APP_PUBLIC_KEY)
-      .then((result) => {
-        setStatus(true)
-        setIsFormSent(true)
-      }, (error) => {
-        setStatus(true);
-        setIsFormSent(false)
-      }); 
-      
+    .then((result) => {
+      setIsFormSent(true)
+    }, (error) => {
+      setIsFormSent(false)
+    }); 
   }
-
-  // const validateCode = () => {
-  //   console.log("Code validation should happen here")
-  // }
-
   
   function handleChange(event, index) {
     const newCode = [...code];
@@ -59,35 +49,33 @@ function App() {
     <div className='grid'>
       <div className="row">
         <div className='col-12 col-md-4 mx-auto mt-5 px-5'>
-          {status.status === 412 ? (
-            <div className="alert alert-success" role="alert">
-              O código foi enviado corretamente.
-            </div>
-          ) : (
-            <div className="alert alert-danger" role="alert">
-              Algo deu errado.
-            </div>
-            ) 
-          }
+          <div className="alert alert-success" role="alert">
+            O código foi enviado corretamente.
+          </div>
+          <div className="alert alert-danger" role="alert">
+            Algo deu errado.
+          </div>
           {!isFormSent ? (
             <form ref={form} onSubmit={sendEmail}>
               <h3 className='mb-4'>Two-factor authentication</h3>
               <div className="mb-3">
                 <label htmlFor="email" className="form-label">Email</label>
-                <input name="email" onChange={(e) => setEmail(e.target.value)} className="form-control" />
+                <input name="email" onChange={(e) => setUserEmail(e.target.value)} className="form-control" />
                 <div className="form-text">We'll never share your email with anyone else.</div>
               </div>
               <button type="submit" className="btn btn-primary">Submit</button>
             </form>
           ) : (
             <form onSubmit={handleSubmit}>
-              <input type="text" value={code[0]} maxLength={1} onChange={event => handleChange(event, 0)} onPaste={handlePaste} required />
-              <input type="text" value={code[1]} maxLength={1} onChange={event => handleChange(event, 1)} required />
-              <input type="text" value={code[2]} maxLength={1} onChange={event => handleChange(event, 2)} required />
-              <input type="text" value={code[3]} maxLength={1} onChange={event => handleChange(event, 3)} required />
-              <input type="text" value={code[4]} maxLength={1} onChange={event => handleChange(event, 4)} required />
-              <input type="text" value={code[5]} maxLength={1} onChange={event => handleChange(event, 5)} required />
-              <button type="submit">Submit</button>
+              <div className='row mx-0 mb-3'>
+                <input type="text" value={code[0]} maxLength={1} onChange={event => handleChange(event, 0)} onPaste={handlePaste} required className="form-control col"/>
+                <input type="text" value={code[1]} maxLength={1} onChange={event => handleChange(event, 1)} required className="form-control col"/>
+                <input type="text" value={code[2]} maxLength={1} onChange={event => handleChange(event, 2)} required className="form-control col"/>
+                <input type="text" value={code[3]} maxLength={1} onChange={event => handleChange(event, 3)} required className="form-control col"/>
+                <input type="text" value={code[4]} maxLength={1} onChange={event => handleChange(event, 4)} required className="form-control col"/>
+                <input type="text" value={code[5]} maxLength={1} onChange={event => handleChange(event, 5)} required className="form-control col"/>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
             </form>
           )}
         </div>
