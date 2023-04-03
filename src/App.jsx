17f9132075559349100e9661e8a8a2816781437c
Fernanda Freitas <idea.fernanda@gmail.com>
2023-04-03 12:21:41 +0100

    corrected validation

--- src/App.jsx
+++ src/App.jsx
@@ -51,29 +51,29 @@ function App() {
       <div className="row">
         <div className='col-12 col-md-4 mx-auto mt-5 px-5'>
           {result && <Alert type={result.type}/>}
-          {!result ? (
-            <form ref={form} onSubmit={sendEmail}>
-              <h3 className='mb-4'>Two-factor authentication</h3>
-              <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email</label>
-                <input name="email" onChange={(e) => setUserEmail(e.target.value)} className="form-control" />
-                <div className="form-text">We'll never share your email with anyone else.</div>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-          ) : (
+          {result && result.type == 200 ? (
             <form onSubmit={handleSubmit}>
-              <h3 className='mb-4'>Two-factor authentication</h3>
-              <div className='row mb-3'>
-                <input type="text" value={code[0]} maxLength={1} onChange={e => handleChange(e, 0)} onPaste={handlePaste} required className="form-control col form-control col mx-2"/>
-                <input type="text" value={code[1]} maxLength={1} onChange={e => handleChange(e, 1)} required className="form-control col form-control col mx-2"/>
-                <input type="text" value={code[2]} maxLength={1} onChange={e => handleChange(e, 2)} required className="form-control col form-control col mx-2"/>
-                <input type="text" value={code[3]} maxLength={1} onChange={e => handleChange(e, 3)} required className="form-control col form-control col mx-2"/>
-                <input type="text" value={code[4]} maxLength={1} onChange={e => handleChange(e, 4)} required className="form-control col form-control col mx-2"/>
-                <input type="text" value={code[5]} maxLength={1} onChange={e => handleChange(e, 5)} required className="form-control col form-control col mx-2"/>
-              </div>
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            <h3 className='mb-4'>Two-factor authentication</h3>
+            <div className='row mb-3'>
+              <input type="text" value={code[0]} maxLength={1} onChange={e => handleChange(e, 0)} onPaste={handlePaste} required className="form-control col form-control col mx-2"/>
+              <input type="text" value={code[1]} maxLength={1} onChange={e => handleChange(e, 1)} required className="form-control col form-control col mx-2"/>
+              <input type="text" value={code[2]} maxLength={1} onChange={e => handleChange(e, 2)} required className="form-control col form-control col mx-2"/>
+              <input type="text" value={code[3]} maxLength={1} onChange={e => handleChange(e, 3)} required className="form-control col form-control col mx-2"/>
+              <input type="text" value={code[4]} maxLength={1} onChange={e => handleChange(e, 4)} required className="form-control col form-control col mx-2"/>
+              <input type="text" value={code[5]} maxLength={1} onChange={e => handleChange(e, 5)} required className="form-control col form-control col mx-2"/>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+          ) : (
+          <form ref={form} onSubmit={sendEmail}>
+            <h3 className='mb-4'>Two-factor authentication</h3>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email</label>
+              <input name="email" onChange={(e) => setUserEmail(e.target.value)} className="form-control" />
+              <div className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
           )}
         </div>
       </div>
