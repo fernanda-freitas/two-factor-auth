ee9f9b339b1ec756594d791c272fcc8f13d5d628
Fernanda Freitas <idea.fernanda@gmail.com>
2023-04-04 17:31:32 +0100

    added loged in image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import Alert from './Alert'
+import Chick from './images/chicken.png'
 
 function App() {
   const form = useRef();
@@ -8,6 +9,8 @@ function App() {
   const [code, setCode] = useState(['', '', '', '', '', ''])
   const [result, setResult] = useState(null)
   const [isVisible, setIsVisible] = useState(false)
+  const [generatedCode, setGeneratedCode] = useState('')
+  const [showChick, setShowChick] = useState(false)
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -21,6 +24,8 @@ function App() {
     }, (error) => {
       setResult({type: error.status})
     }); 
+
+    setGeneratedCode(templateParams.generated_code)
   }
 
   useEffect(() => {
@@ -51,9 +56,13 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    const enteredCode = code.join('');
-    // Add code validation logic here
-    alert(`Entered code: ${enteredCode}`);
+    const enteredCode = Number(code.join(''));
+    if (generatedCode === enteredCode) {
+      console.log('code matches')
+      setShowChick(true)
+    } else {
+      console.log('wrong code')
+    }
   }
 
   return (
@@ -63,17 +72,17 @@ function App() {
           {result && isVisible && <Alert type={result.type}/>}
           {result && result.type === 200 ? (
             <form onSubmit={handleSubmit}>
-            <h3 className='mb-4'>Two-factor authentication</h3>
-            <div className='row mb-3'>
-              <input type="text" value={code[0]} maxLength={1} onChange={e => handleChange(e, 0)} onPaste={handlePaste} required className="form-control col form-control col mx-2"/>
-              <input type="text" value={code[1]} maxLength={1} onChange={e => handleChange(e, 1)} required className="form-control col form-control col mx-2"/>
-              <input type="text" value={code[2]} maxLength={1} onChange={e => handleChange(e, 2)} required className="form-control col form-control col mx-2"/>
-              <input type="text" value={code[3]} maxLength={1} onChange={e => handleChange(e, 3)} required className="form-control col form-control col mx-2"/>
-              <input type="text" value={code[4]} maxLength={1} onChange={e => handleChange(e, 4)} required className="form-control col form-control col mx-2"/>
-              <input type="text" value={code[5]} maxLength={1} onChange={e => handleChange(e, 5)} required className="form-control col form-control col mx-2"/>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
+              <h3 className='mb-4'>Two-factor authentication</h3>
+              <div className='row mb-3'>
+                <input type="text" value={code[0]} maxLength={1} onChange={e => handleChange(e, 0)} onPaste={handlePaste} required className="form-control col form-control mx-2"/>
+                <input type="text" value={code[1]} maxLength={1} onChange={e => handleChange(e, 1)} required className="form-control col form-control mx-2"/>
+                <input type="text" value={code[2]} maxLength={1} onChange={e => handleChange(e, 2)} required className="form-control col form-control mx-2"/>
+                <input type="text" value={code[3]} maxLength={1} onChange={e => handleChange(e, 3)} required className="form-control col form-control mx-2"/>
+                <input type="text" value={code[4]} maxLength={1} onChange={e => handleChange(e, 4)} required className="form-control col form-control mx-2"/>
+                <input type="text" value={code[5]} maxLength={1} onChange={e => handleChange(e, 5)} required className="form-control col form-control mx-2"/>
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
           ) : (
           <form ref={form} onSubmit={sendEmail}>
             <h3 className='mb-4'>Two-factor authentication</h3>
@@ -85,6 +94,7 @@ function App() {
             <button type="submit" className="btn btn-primary">Submit</button>
           </form>
           )}
+          {showChick && <img src={Chick} alt="Chick image after a happy authentication." /> }
         </div>
       </div>
     </div>
